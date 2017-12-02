67c000e7220429e222f0771863d916418f386c0e
Byron Chang <thebyronc@gmail.com>
2017-12-01 17:03:05 -0800

    fix spelling and grammar errors

--- js/scripts.js
+++ js/scripts.js
@@ -18,22 +18,22 @@ $(document).ready(function() {
     var combinedInput = q1Input + q2Input + q3Input + q4Input + q5Input;
 
     if (q5Input === 5) {
-      $("#output").text("Hello " + nameInput + ", it is highly recommended that you take the PHP/Drupal course from Epicodus. Based on your response, taking this course will allow you to focus on backend coding.");
+      $("#output").text("Hello " + nameInput + ", we highly recommended that you take the PHP/Drupal course from Epicodus. Based on your responses, taking this course will allow you to focus on backend coding.");
       makeVisible();
       $("#couresrec").text("PHP/Drupal");
       $(".phpdrupal").fadeIn();
     } else if (combinedInput <= 14) {
-      $("#output").text("Hello " + nameInput + ", It is recommended that you take the CSS/Design course from Epicodus. Based on your response, taking this course will allow you to focus on developing for a web interface.");
+      $("#output").text("Hello " + nameInput + ", we recommended that you take the CSS/Design course from Epicodus. Based on your responses, taking this course will allow you to focus on developing for a web interface.");
       $("#couresrec").text("CSS/Design");
       makeVisible();
       $(".cssdesign").fadeIn();
     } else if (combinedInput >= 16) {
-      $("#output").text("Hello " + nameInput + ", It is recommended that you take the Java/Android course from Epicodus. Based on your response, taking this course will allow you to focus on developing for a mobile interface.");
+      $("#output").text("Hello " + nameInput + ", we recommended that you take the Java/Android course from Epicodus. Based on your responses, taking this course will allow you to focus on developing for a mobile interface.");
       $("#couresrec").text("Java/Android");
       makeVisible();
       $(".javaandroid").fadeIn();
     } else if (combinedInput === 15) {
-      $("#output").text("Hello " + nameInput + ", You seem to not seem to have a particular interest in either mobile development or desktop development. May we suggest the PHP/Drupal course from Epicodus.");
+      $("#output").text("Hello " + nameInput + ", you seem to not have a particular interest in either mobile development or desktop development. We suggest the PHP/Drupal course from Epicodus.");
       $("#couresrect").text("PHP/Drupal");
       makeVisible();
       $(".phpdrupal").fadeIn();
